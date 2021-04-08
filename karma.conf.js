989b307ca4ac73408f882922bc64ce44addafc11
Adam <selvaggio.adam@gmail.com>
2021-04-08 16:24:48 +0200

    testing

--- karma.conf.js
+++ karma.conf.js
@@ -27,12 +27,19 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-	browsers: ['ChromeHeadlessNoSandbox'],
+	browsers: ["HeadlessChrome"],
     customLaunchers: {
-        ChromeHeadlessNoSandbox: {
-            base: 'ChromeHeadless',
-            flags: ['--no-sandbox']
-        }
-    }
+      HeadlessChrome: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox", // required to run without privileges in Docker
+          "--disable-web-security",
+          "--disable-gpu",
+          "--remote-debugging-port=9222"
+        ]
+      }
+    },
+    singleRun: true,
+    autoWatch: false,
   });
 };
