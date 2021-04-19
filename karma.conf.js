5064a2eac2e5650af646b24c22878bfb28fd866c
Adam <selvaggio.adam@gmail.com>
2021-04-19 10:42:04 +0200

    try headless

--- karma.conf.js
+++ karma.conf.js
@@ -4,37 +4,27 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-junit-reporter'),
+      require('@angular/cli/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular10FirebaseStorage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+ reporters: ['junit'],    
+    junitReporter: { 
+ outputDir: 'karma-results',    
+ outputFile: 'karma-results.xml'    
     },
     angularCli: {
-      environment: 'dev'
+      environment: 'dev',
     },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    singleRun: false,
-    browsers: ['Chrome'],
-//     customLaunchers: {
-//     Chromium_no_sandbox: {
-//     base: 'ChromiumHeadless',
-//     flags: ['--no-sandbox']
-//   }
-// },
+ browsers: ['ChromeHeadless'],    
+ autoWatch: false,    
+ singleRun: true    
   });
 };
+
