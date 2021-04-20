7fe476552f29fb761efce355b73fe016f9469f5a
Adam <selvaggio.adam@gmail.com>
2021-04-20 09:31:23 +0200

    update karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -14,8 +14,14 @@ module.exports = function (config) {
     ],
   reporters: ['junit'],    
     junitReporter: { 
-  outputDir: 'karma-results',    
-  outputFile: 'karma-results.xml'    
+	  outputDir: 'karma-results',    
+	  outputFile: 'karma-results.xml',
+	  suite: '', // suite will become the package name attribute in xml testsuite element
+	  useBrowserName: true, // add browser name to report and classes names
+	  nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+	  classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+	  properties: {}, // key value pair of properties to add to the <properties> section of the report
+	  xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format	    
     },
     angularCli: {
       environment: 'dev',
