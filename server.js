666ecde06f54528a1fdc028c616b0e5500c1bd8b
QuentinReynaud <quentin.reynaud99@gmail.com>
2021-04-22 12:46:16 +0200

    path installed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 let port = process.env.PORT || 3001;
 const fs = require('fs');
